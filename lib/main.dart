1ace3e8e7bf7c37048d01074ecced791fb6c6993
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2024-09-07 17:38:11 +0900

    [add] 現在地の取得を実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
@@ -11,13 +13,23 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+enum LocationSettingResult {
+  serviceDisabled,
+  permissionDenied,
+  permissionDeniedForever,
+  enabled,
+}
+
 class _MyAppState extends State<MyApp> {
   final Set<Marker> _markers = {};
+  CameraPosition? _currentPosition;
 
   @override
   void initState() {
     super.initState();
     _loadCustomMarker();
+    _initLocationService();
+    _setInitialLocation();
   }
 
   late GoogleMapController mapController;
@@ -47,17 +59,29 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<void> _setInitialLocation() async {
+    final LatLng currentLocation = await getCurrentLocation();
+    setState(() {
+      _currentPosition = CameraPosition(
+        bearing: 192.8334901395799,
+        target: currentLocation,
+        tilt: 59.440717697143555,
+        zoom: 19.151926040649414,
+      );
+    });
+  }
+
+
   final LatLng _center = const LatLng(26.4874273, 127.5783151);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  Future<LatLng> getCurrentLocation() async {
+    final position = await Geolocator.getCurrentPosition();
+    return LatLng(position.latitude, position.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,16 +101,66 @@ class _MyAppState extends State<MyApp> {
           markers: _markers,
         ),
         floatingActionButton: FloatingActionButton.extended(
-            onPressed: _goToTheLake,
-            label: const Text('To the lake!'),
-            icon: const Icon(Icons.directions_boat),
+          onPressed: _goToTheCurrentLocation,
+          label: const Text('現在地へGo!'),
+          icon: const Icon(Icons.directions_boat),
         ),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToTheCurrentLocation() async {
     // final GoogleMapController controller = await mapController.future;
-    mapController.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    mapController.animateCamera(CameraUpdate.newCameraPosition(_currentPosition!));
   }
+
+  Future<void> _initLocationService() async {
+    final locationResult = await checkLocationSetting();
+    await recoverLocationSettings(context, locationResult);
+  }
+
+  // 位置情報に関するパーミションを取得するメソッド
+  Future<LocationSettingResult> checkLocationSetting() async {
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.value(LocationSettingResult.serviceDisabled);
+    }
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.value(LocationSettingResult.permissionDenied);
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.value(LocationSettingResult.permissionDeniedForever);
+    }
+    return Future.value(LocationSettingResult.enabled);
+  }
+
+  // 位置情報を許可してもらうようにまた表示する
+  Future<void> recoverLocationSettings(
+      BuildContext context, LocationSettingResult locationResult) async {
+    if (locationResult == LocationSettingResult.enabled) {
+      return;
+    }
+    final result = await showOkCancelAlertDialog(
+      context: context,
+      okLabel: 'OK',
+      cancelLabel: 'キャンセル',
+      title: 'xxxxxxx',
+      message: 'xxxxxxxxxxxx',
+    );
+    if (result == OkCancelResult.cancel) {
+        // キャンセルされた時の処理
+    } else {
+      // 許可の状況に応じて、設定を開いて許可してもらう
+      locationResult == LocationSettingResult.serviceDisabled
+          ? await Geolocator.openLocationSettings()
+          : await Geolocator.openAppSettings();
+    }
+  }
+
+
 }
