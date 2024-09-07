6977efb478cf73c763fc48d246d57639b967a0b6
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2024-09-07 16:01:15 +0900

    [add] floatingActionButton, markerの追加

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,12 @@ class _MyAppState extends State<MyApp> {
     mapController = controller;
   }
 
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,12 +36,33 @@ class _MyAppState extends State<MyApp> {
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
+          mapType: MapType.hybrid, // 方位磁針
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 11.0,
           ),
+          markers: {
+            const Marker(
+                markerId: MarkerId('Santiago'),
+                position: LatLng(-33.488897, -70.669265)
+            ),
+            const Marker(
+                markerId: MarkerId('Sydney'),
+                position: LatLng(-33.86, 151.20),
+            )
+          },
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: _goToTheLake,
+            label: const Text('To the lake!'),
+            icon: const Icon(Icons.directions_boat),
         ),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    // final GoogleMapController controller = await mapController.future;
+    mapController.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
